dca175971d6872586e6669ec9fc971d269c7dde4
Steveinfant05 <steveinfant@karunya.edu.in>
2025-05-23 11:42:47 +0530

    v0.2

--- src/App.js
+++ src/App.js
@@ -1,121 +1,85 @@
-import React from 'react'
-import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom'
-import './App.css'
-import Login from './component/Login'
-import Home from './component/Home'
-import ProtectedRoute from './component/ProtectedRoute'
-import Trending from './component/Trending'
-import Gaming from './component/Gaming'
-import SavedV from './component/SavedV'
-import NotFoundH from './component/NotFoundH'
-import VideoDetails from './component/VideoDetails'
-import ThemeContext from './component/Contexts/ThemeContext'
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import VideoDetailView from './components/VideoDetailView'
+import TrendingVideos from './components/TrendingVideos'
+import GamingVideos from './components/GamingVideos'
+import SavedVideos from './components/SavedVideos'
+import NotFound from './components/NotFound'
 
-class App extends React.Component {
+import ThemeAndVideoContext from './context/ThemeAndVideoContext'
+
+import './App.css'
+
+// Replace your code here
+
+class App extends Component {
   state = {
-    isDarkTheme: false,
-    selectedMenu: 'HOME',
     savedVideos: [],
-    likedVideos: [],
-    dislikedVideos: [],
+    isDarkTheme: false,
+    activeTab: 'Home',
+  }
+
+  changeTab = tab => {
+    this.setState({activeTab: tab})
   }
 
   toggleTheme = () => {
-    this.setState(prevState => ({isDarkTheme: !prevState.isDarkTheme}))
-  }
-
-  setSelectedMenu = menu => {
-    this.setState({selectedMenu: menu})
-  }
-
-  addSavedVideo = video => {
     this.setState(prevState => ({
-      savedVideos: [...prevState.savedVideos, video],
+      isDarkTheme: !prevState.isDarkTheme,
     }))
   }
 
-  removeSavedVideo = videoId => {
-    this.setState(prevState => ({
-      savedVideos: prevState.savedVideos.filter(video => video.id !== videoId),
-    }))
+  addVideo = video => {
+    const {savedVideos} = this.state
+    const index = savedVideos.findIndex(eachVideo => eachVideo.id === video.id)
+    if (index === -1) {
+      this.setState({savedVideos: [...savedVideos, video]})
+    } else {
+      savedVideos.splice(index, 1)
+      this.setState({savedVideos})
+    }
   }
 
-  addLikedVideo = videoId => {
-    this.setState(prevState => ({
-      dislikedVideos: prevState.dislikedVideos.filter(id => id !== videoId),
-      likedVideos: [...prevState.likedVideos, videoId],
-    }))
-  }
-
-  addDislikedVideo = videoId => {
-    this.setState(prevState => ({
-      likedVideos: prevState.likedVideos.filter(id => id !== videoId),
-      dislikedVideos: [...prevState.dislikedVideos, videoId],
-    }))
-  }
-
-  removeLikedVideo = videoId => {
-    this.setState(prevState => ({
-      likedVideos: prevState.likedVideos.filter(id => id !== videoId),
-    }))
-  }
-
-  removeDislikedVideo = videoId => {
-    this.setState(prevState => ({
-      dislikedVideos: prevState.dislikedVideos.filter(id => id !== videoId),
-    }))
+  removeVideo = id => {
+    const {savedVideos} = this.state
+    const updatedSavedVideos = savedVideos.filter(
+      eachVideo => eachVideo.id !== id,
+    )
+    this.setState({savedVideos: updatedSavedVideos})
   }
 
   render() {
-    const {
-      isDarkTheme,
-      selectedMenu,
-      savedVideos,
-      likedVideos,
-      dislikedVideos,
-    } = this.state
-
-    console.log({
-      Home,
-      ProtectedRoute,
-      Trending,
-      Gaming,
-      SavedV,
-      NotFoundH,
-      VideoDetails,
-    })
-
+    const {savedVideos, isDarkTheme, activeTab} = this.state
+    // console.log(savedVideos)
     return (
-      <BrowserRouter>
-        <ThemeContext.Provider
-          value={{
-            isDarkTheme,
-            selectedMenu,
-            toggleTheme: this.toggleTheme,
-            setSelectedMenu: this.setSelectedMenu,
-            savedVideos,
-            addSavedVideo: this.addSavedVideo,
-            removeSavedVideo: this.removeSavedVideo,
-            likedVideos,
-            addLikedVideo: this.addLikedVideo,
-            dislikedVideos,
-            addDislikedVideo: this.addDislikedVideo,
-            removeLikedVideo: this.removeLikedVideo,
-            removeDislikedVideo: this.removeDislikedVideo,
-          }}
-        >
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <ProtectedRoute exact path="/" component={Home} />
-            <ProtectedRoute exact path="/trending" component={Trending} />
-            <ProtectedRoute exact path="/gaming" component={Gaming} />
-            <ProtectedRoute exact path="/saved-videos" component={SavedV} />
-            <ProtectedRoute path="/videos/:id" component={VideoDetails} />
-            <Route path="/not-found" component={NotFoundH} />
-            <Redirect to="/not-found" />
-          </Switch>
-        </ThemeContext.Provider>
-      </BrowserRouter>
+      <ThemeAndVideoContext.Provider
+        value={{
+          savedVideos,
+          isDarkTheme,
+          activeTab,
+          toggleTheme: this.toggleTheme,
+          addVideo: this.addVideo,
+          changeTab: this.changeTab,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute
+            exact
+            path="/videos/:id"
+            component={VideoDetailView}
+          />
+          <ProtectedRoute exact path="/trending" component={TrendingVideos} />
+          <ProtectedRoute exact path="/gaming" component={GamingVideos} />
+          <ProtectedRoute exact path="/saved-videos" component={SavedVideos} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </ThemeAndVideoContext.Provider>
     )
   }
 }
